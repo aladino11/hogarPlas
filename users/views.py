8e27a3a794660269367e2631963c8f9e52aaefed
Cristian Quiza <cristian.david_95@hotmail.com>
2022-03-31 00:32:07 -0500

    Configuracion Bases de Datos

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+ 
 # Create your views here.
+
+def users_list(request):
+    return render(request,"users/users_list.html")
+
+def users_form(request):
+    return render(request,"users/users_form.html")
+
+def users_delete(request):
+    return
